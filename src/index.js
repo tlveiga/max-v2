26c47071808c90cdee487d121b499cfb3e9753c1
Tiago Veiga <desandador@gmail.com>
2019-10-28 13:16:37 +0000

    more styles, more html, more js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import './style.css'
+import axios from 'axios';
 
+function setElementHtml(name, html) {
+  var elem = document.getElementById(name);
+  elem.innerHTML = html;
+}
 
-var deviceTime = null;
-setInterval(function () {
-  if (deviceTime) {
-    var time = document.getElementById("time");
-    time.innerText = new Date().toLocaleString();
-  }
-}, 100);
+function setElementValue(name, value) {
+  var elem = document.getElementById(name);
+  elem.value = value;
+}
+
+function setCheckbox(name, value) {
+  var elem = document.getElementById(name);
+  elem.checked = value;
+}
 
 function getInfo() {
-  var name = document.getElementById("name");
-  name.innerText = "MAX";
-  deviceTime = 1;
+  setElementValue("name", "MAX");
+  setElementHtml("version", "0.0.0");
+  setElementValue("updateServer", "http://update.me");
+  setElementHtml("availableVersion", "1.0.0");
+}
+
+function getMQTT() {
+  setElementValue("mqtt_server", "http://my.mqtt.broker.url");
+  setElementValue("mqtt_topic", "mqtt_topic");
+  setCheckbox("mqtt_active", 1);
 }
 
 
 document.body.onload = function () {
   document.body.removeAttribute("cloak");
   getInfo();
+  getMQTT();
 };
